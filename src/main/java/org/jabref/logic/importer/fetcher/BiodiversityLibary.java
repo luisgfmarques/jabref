8cc29da9c5263a189eb391c0c6b2779de41f70b3
Luis Marques <LuisGustavoMarques@outlook.com>
2022-04-05 23:20:23 -0300

    update files

---
+++ src/main/java/org/jabref/logic/importer/fetcher/BiodiversityLibary.java
@@ -0,0 +1,16 @@
+package org.jabref.logic.importer.fetcher;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import com.google.gson.Gson;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BiodiversityLibary {
+	
+	
+}
---
+++ src/test/java/org/jabref/logic/importer/fetcher/BiodiversityLibaryTest.java
@@ -0,0 +1,23 @@
+package org.jabref.logic.importer.fetcher;
+
+import org.jabref.testutils.category.FetcherTest;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@FetcherTest
+class BiodiversityLibraryTest {
+
+    @Test
+    void getFetchDataShouldReturnJsonWithDataTest() throws Exception {
+    	fail();
+    }
+
+    @Test
+    void getFetchDataShouldThrowExceptionTest() throws Exception {
+        fail();
+    }
+}
